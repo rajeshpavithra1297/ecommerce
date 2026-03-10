be4d9b1f0364240328293c4d0c4fa22008995d83
rajeshpavithra1297 <265609470+rajeshpavithra1297@users.noreply.github.com>
2026-03-10 16:03:40 -0300

    second commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
+import {BrowserRouter} from 'react-router'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>,
+  </StrictMode>
+ 
 )
