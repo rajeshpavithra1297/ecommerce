47bd0387e712a8994cb3ed035b9e099277e7931b
rajeshpavithra1297 <265609470+rajeshpavithra1297@users.noreply.github.com>
2026-03-11 14:41:39 -0300

    use data from backened to generate HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,14 +1,21 @@
 import axios from 'axios'
 import { Header } from '../components/Header';
 import './HomePage.css'
+import { useEffect, useState } from 'react';
 
 export function HomePage(){
+  
+  const[products, setProducts]= useState([]);
+  
+  
+  useEffect(()=>{
+    axios.get('http://localhost:3000/api/products')
+    .then((response)=>{
+        setProducts(response.data);
+     
+    })
+  },[])
 
-  axios.get('http://localhost:3000/api/products')
-  .then((response)=>{
-      console.log(response.data);
-   
-  })
 
     return(
         <>
@@ -17,26 +24,29 @@ export function HomePage(){
         <Header/>
             <div class="home-page">
       <div class="products-grid">
-        <div class="product-container">
+
+        {products.map((product)=>{
+          return(
+            <div key={product.id}class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="images/products/athletic-cotton-socks-6-pairs.jpg" />
+              src={product.image} />
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-            Black and Gray Athletic Cotton Socks - 6 Pairs
+            {product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-45.png" />
+              src={`images/ratings/rating-${product.rating.stars*10}.png`} />
             <div class="product-rating-count link-primary">
-              87
+              {product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $10.90
+           $ {(product.priceCents/100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
@@ -65,104 +75,12 @@ export function HomePage(){
             Add to Cart
           </button>
         </div>
+          )
 
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="images/products/intermediate-composite-basketball.jpg" />
-          </div>
+        })}
+        
 
-          <div class="product-name limit-text-to-2-lines">
-            Intermediate Size Basketball
-          </div>
 
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-40.png" />
-            <div class="product-rating-count link-primary">
-              127
-            </div>
-          </div>
-
-          <div class="product-price">
-            $20.95
-          </div>
-
-          <div class="product-quantity-container">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png" />
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
-        </div>
-
-        <div class="product-container">
-          <div class="product-image-container">
-            <img class="product-image"
-              src="images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg" />
-          </div>
-
-          <div class="product-name limit-text-to-2-lines">
-            Adults Plain Cotton T-Shirt - 2 Pack
-          </div>
-
-          <div class="product-rating-container">
-            <img class="product-rating-stars"
-              src="images/ratings/rating-45.png" />
-            <div class="product-rating-count link-primary">
-              56
-            </div>
-          </div>
-
-          <div class="product-price">
-            $7.99
-          </div>
-
-          <div class="product-quantity-container">
-            <select>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-              <option value="6">6</option>
-              <option value="7">7</option>
-              <option value="8">8</option>
-              <option value="9">9</option>
-              <option value="10">10</option>
-            </select>
-          </div>
-
-          <div class="product-spacer"></div>
-
-          <div class="added-to-cart">
-            <img src="images/icons/checkmark.png" />
-            Added
-          </div>
-
-          <button class="add-to-cart-button button-primary">
-            Add to Cart
-          </button>
-        </div>
       </div>
             </div>
         </>
